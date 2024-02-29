2077726356ed8bdeb3a310ab401d823ec0591a63
Maurício Dias de Carvalho Machado <mauricio.machado@antt.gov.br>
2024-02-29 19:33:11 -0300

    Adicionar Fotos

---
+++ Models/Anexo.cs
@@ -0,0 +1,21 @@
+﻿using app_Tarefas.Enums;
+using app_Tarefas.Servicos;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_Tarefas.Models
+{
+    public class Anexo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Arquivo { get; set; }
+        public int TarefaId { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
--- Paginas/TarefasDetalhePage.xaml.cs
+++ Paginas/TarefasDetalhePage.xaml.cs
@@ -11,6 +11,8 @@ public partial class TarefasDetalhePage : ContentPage
     private DataBaseServico<Tarefa> _tarefaservico;
 
     private DataBaseServico<Comentario> _comentarioservico;
+
+    private DataBaseServico<Anexo> _anexoservico;
     public TarefasDetalhePage(Tarefa tarefa)
 	{
 		InitializeComponent();
@@ -18,6 +20,7 @@ public partial class TarefasDetalhePage : ContentPage
 		BindingContext = this;
         _tarefaservico = new DataBaseServico<Tarefa>(Db.DB_PATH);
         _comentarioservico = new DataBaseServico<Comentario>(Db.DB_PATH);
+        _anexoservico = new DataBaseServico<Anexo>(Db.DB_PATH);
 
     }
 
@@ -33,6 +36,7 @@ public partial class TarefasDetalhePage : ContentPage
         UsuarioPicker.ItemsSource = UsuarioServico.Instancia().Todos();
 
         CarregarComentarios();
+        CarregarImagens();
     }
 
     private async void AdicionarComentarioClicked(object sender, EventArgs e)
@@ -61,6 +65,69 @@ public partial class TarefasDetalhePage : ContentPage
     private async void CarregarComentarios()
     {
         ComentariosCollection.ItemsSource = await _comentarioservico.Query().Where(c => c.TarefaId == Tarefa.Id).ToListAsync();
+    }
+
+    private async void CarregarImagens()
+    {
+        var fotos = await _anexoservico.Query().Where(a => a.TarefaId == Tarefa.Id && !string.IsNullOrEmpty(a.Arquivo)).ToListAsync();
+        if(fotos.Count > 0)
+        {
+            FotosCollection.IsVisible = true;
+            FotosCollection.ItemsSource = fotos;
+            return;
+        }
+        FotosCollection.IsVisible = false;
+    }
+
+    private async void TirarFotoClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            //Verifica se a câmera está disponível no dispositivo
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                //Tira uma foto e obtem o arquivo resultante
+                FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+
+                if (photo != null)
+                {  
+                    //Cria um stream a partir do arquivo da foto
+                    using Stream stream = await photo.OpenReadAsync();
+
+                    //Define o Diretório e o nome aonde a foto será salva
+                    string directory = FileSystem.AppDataDirectory;
+                    string filename = Path.Combine(directory, $"{DateTime.Now.ToString("ddMMyyyy_hhmmss")}.jpg");
+
+                    //Salva a foto no diretório definido
+                    using FileStream fileStream = new FileStream(filename, FileMode.Create, FileAccess.Write);
+                    await stream.CopyToAsync(fileStream);
+
+                    await _anexoservico.IncluirAsync(new Anexo
+                    {
+                        Arquivo = filename,
+                        TarefaId = Tarefa.Id,
+                    });
+
+                    CarregarImagens();
+                }
+            }
+            else
+            {
+                await DisplayAlert("Erro", "A Captura de Fotos não é suportada nesse dispositivo", "Fechar");
+            }
+        }
+        catch (FeatureNotSupportedException fnsex)
+        {
+            await DisplayAlert("Erro", "A Captura de Fotos não é suportada nesse dispositivo. - " + fnsex.Message, "Ok");
+        }
+        catch(PermissionException pex)
+        {
+            await DisplayAlert("Erro", "Permissão para Acessar a Câmera não concesida. - " + pex.Message, "Ok");
+        }
+        catch(Exception ex)
+        {
+            await DisplayAlert("Erro", $"Ocorreu um erro: {ex.Message}", "Ok");
+        }
     }
 
     private async void VoltarClicked(object sender, EventArgs e)
