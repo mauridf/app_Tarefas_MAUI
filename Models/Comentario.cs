1210c55b058377b5bda4a92278a5f567961c3390
Maurício Dias de Carvalho Machado <mauricio.machado@antt.gov.br>
2024-02-29 17:15:50 -0300

    Adicionar os Comentários nas Tarefas.

---
+++ Models/Comentario.cs
@@ -0,0 +1,44 @@
+﻿using app_Tarefas.Enums;
+using app_Tarefas.Servicos;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_Tarefas.Models
+{
+    public class Comentario
+    {
+        public Comentario()
+        {
+            this.Data = DateTime.Now;
+        }
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Texto { get; set; }
+        public string Descricao { get; set; }
+        public DateTime Data { get; set; }
+        public int TarefaId { get; set; }
+        public int UsuarioId { get; set; }
+        [Ignore]
+        public Usuario? Usuario
+        {
+            get
+            {
+                if (this.UsuarioId == 0) return null;
+                return UsuarioServico.Instancia().Todos().Find(u => u.Id == this.UsuarioId);
+            }
+        }
+        [Ignore]
+        public string NomeUsuario
+        {
+            get
+            {
+                if (this.Usuario == null) return "Sem Usuário";
+                return Usuario?.Nome;
+            }
+        }
+    }
+}
--- Paginas/TarefasDetalhePage.xaml.cs
+++ Paginas/TarefasDetalhePage.xaml.cs
@@ -1,19 +1,85 @@
 using app_Tarefas.Models;
+using app_Tarefas.Servicos;
+using app_Tarefas.Constantes;
 
 namespace app_Tarefas.Paginas;
 
 public partial class TarefasDetalhePage : ContentPage
 {
 	public Tarefa Tarefa { get; set; }
-	public TarefasDetalhePage(Tarefa tarefa)
+
+    private DataBaseServico<Tarefa> _tarefaservico;
+
+    private DataBaseServico<Comentario> _comentarioservico;
+    public TarefasDetalhePage(Tarefa tarefa)
 	{
 		InitializeComponent();
 		Tarefa = tarefa;
 		BindingContext = this;
-	}
+        _tarefaservico = new DataBaseServico<Tarefa>(Db.DB_PATH);
+        _comentarioservico = new DataBaseServico<Comentario>(Db.DB_PATH);
 
-	private async void VoltarClicked(object sender, EventArgs e)
-	{
-		await Navigation.PopAsync();
-	}
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        LabelTitulo.Text = Tarefa.Titulo;
+        LabelNomeUsuario.Text = Tarefa.NomeUsuario;
+        LabelStatus.Text = Tarefa.Status.ToString();
+        LabelDescricao.Text = Tarefa.Descricao;
+        LabelDataCriacao.Text = Tarefa.DataCriacao.ToString();
+        LabelDataAtualizacao.Text = Tarefa.DataAtualizacao.ToString();
+        UsuarioPicker.ItemsSource = UsuarioServico.Instancia().Todos();
+
+        CarregarComentarios();
+    }
+
+    private async void AdicionarComentarioClicked(object sender, EventArgs e)
+    {
+        if(string.IsNullOrEmpty(NovoComentarioEditor.Text) || UsuarioPicker.SelectedItem == null)
+        {
+            await DisplayAlert("Erro", "Digite o comentário e Selecione o Usuário","Fechar");
+            return;
+        }
+
+        var usuario = (Usuario)UsuarioPicker.SelectedItem;
+
+        await _comentarioservico.IncluirAsync(new Comentario
+        {
+            UsuarioId = usuario.Id,
+            TarefaId = Tarefa.Id,
+            Texto = NovoComentarioEditor.Text
+        });
+
+        NovoComentarioEditor.Text = string.Empty;
+        UsuarioPicker.SelectedItem = -1;
+
+        CarregarComentarios();
+    }
+
+    private async void CarregarComentarios()
+    {
+        ComentariosCollection.ItemsSource = await _comentarioservico.Query().Where(c => c.TarefaId == Tarefa.Id).ToListAsync();
+    }
+
+    private async void VoltarClicked(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
+
+    private async void IrParaAlteracaoClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new TarefasSalvarPage(Tarefa));
+    }
+
+    private async void IrParaExclusaoClicked(object sender, EventArgs e)
+    {
+        bool confirm = await DisplayAlert("Confirmação", "Deseja Excluir essa Tarefa?", "Sim", "Não");
+        if (confirm)
+        {
+            await _tarefaservico.DeleteAsync(Tarefa);
+            await Navigation.PopAsync();
+        }
+    }
 }
